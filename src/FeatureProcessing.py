29c5c51565ac78919d0a1be7cc814e938a3ba939
galuu <bogi2010@gmail.com>
2016-03-12 06:05:00 -0600

    made a few changes to wordToVecConvertor.py and renamed it because didn't want to change old code in case we needed to go back. Result: loading data is 60 times faster (used to take 60 minutes, now it takes 1 minute). Feature adding is also more systematic. No hard-coded dimensions in the code so that more features can be added with minimal code editing. All future features should be added to this file

---
+++ src/FeatureProcessing.py
@@ -0,0 +1,122 @@
+"""
+Feature processing functions
+
+"""
+import pandas as pd
+import numpy as np
+#from nltk.tag import stanford
+
+##
+## All features should be added in this file as a function
+## including POS, etc. 
+##
+
+def readDictData(wordToVecDictFile):
+    """
+    Reads in word2vec dictionary
+    """
+    f = open(wordToVecDictFile, 'r')
+    rawData = f.readlines()
+    wordVecDict = {}
+    for line in rawData:
+        line = line.strip().split()
+        word = line[0]
+        vec = line[1:]
+        wordVecDict[word] = np.array(vec, dtype=float)
+    return wordVecDict
+
+def getWordVector(word, wordVecDict):
+    """
+    Given a word or a PAD vector, returns the word2vec vector or the zero vector
+    """
+    if word != 'PAD' and word.lower() in wordVecDict:
+        return list(wordVecDict[word.lower()])
+    return list(np.zeros_like(wordVecDict['hi']))
+
+
+def ContextTokens(tokenized_sentence, token, window_size):
+    """
+    Given a token in a sentence, returns (window_size) tokens before and after. 
+
+    :param tokenized_sentence: list of tokens that make up a sentence
+    :param token: the current token for which we want the context words
+    :window_size: how many words before and after the current token to take
+
+    Output: list of tokens of size 2*(window_size)
+    """
+    token_pos = tokenized_sentence.index(token) #position of the token in the sentence
+    n_sentence = len(tokenized_sentence) 
+    #print token_pos, n_sentence
+    frontContext_tokens = []
+    backContext_tokens = []
+    for i in range(window_size):
+        current_front_vector = token_pos - window_size + i
+        #print "current_front_vector is %d" %current_front_vector
+        
+        if current_front_vector < 0 :
+            frontContext_tokens.append('PAD')
+        else:
+            frontContext_tokens.append(tokenized_sentence[current_front_vector])
+            #print "current front token is %s" %tokenized_sentence[current_front_vector]
+            
+        current_back_vector = token_pos + i + 1
+        #print "current_back_vector is %d" %current_back_vector
+        
+        if current_back_vector >= n_sentence:
+            backContext_tokens.append('PAD')
+        else:
+            backContext_tokens.append(tokenized_sentence[current_back_vector])
+            #print "current back token is %s" %tokenized_sentence[current_back_vector]
+        #window_size = window_size - 1
+    return frontContext_tokens + backContext_tokens
+
+# isUpper
+def isWordUpper(word):
+    if word[0].isupper():
+        return [1]
+    else:
+        return [0]
+
+# label vector given by an index
+def getLabelIndex(label):
+    labList = ['B-Contact-Info','B-Crime','B-FAC','B-GPE','B-Job-Title','B-LOC','B-Numeric','B-ORG','B-PER','B-Sentence','B-TIME','B-VEH','B-WEA','I-Contact-Info','I-Crime','I-FAC','I-GPE','I-Job-Title','I-LOC','I-Numeric','I-ORG','I-PER','I-Sentence','I-TIME','I-VEH','I-WEA','O']
+    return labList.index(label)
+
+def featureProcess(DataFile,LabelFile,wordToVecDictFile,window_size):
+    """
+    Given data and label files, returns a list of lists of length the
+    number of tokens in the data. For each token, outputs a feature vector
+    and a label int (corresponding to the index)
+    """
+    rawTextData = open(DataFile, 'r').readlines()
+    rawLabelData = open(LabelFile, 'r').readlines()
+    wordVecDict = readDictData(wordToVecDictFile)
+    #st = StanfordPOSTagger('/Users/bolor/stanford-postagger-2015-12-09/models/english-bidirectional-distsim.tagger','/Users/bolor/stanford-postagger-2015-12-09/stanford-postagger.jar')
+    # I don't think we want to hard code the dimension of X since we will add
+    # more features in the future
+    #feature_size = (2*window_size + 1)*word_vec_dim + 1
+    XX = []
+    YY = []
+    #XX = np.zeros([1,feature_size])
+    #YY = np.zeros([1,1])
+    ln = 1
+    for line,label in zip(rawTextData,rawLabelData):
+        print "reading line %d" %ln
+        #tokenized_sentence = nltk.word_tokenizer(line)
+        tokenized_sentence = line.strip().split()
+        #POS_tags = st.tag(tokenized_sentence)
+        tokenized_y = label.strip().split()
+        if len(tokenized_sentence) != len(tokenized_y):
+            raise NotImplementedError
+        for token, y in zip(tokenized_sentence,tokenized_y):
+            X = isWordUpper(token) + getWordVector(token,wordVecDict)
+            context = ContextTokens(tokenized_sentence,token,window_size)
+            for context_token in context:
+                X = X + getWordVector(context_token,wordVecDict)
+            print len(X),y,getLabelIndex(y)
+            XX.append(X)
+            YY.append(getLabelIndex(y))
+        ln += 1
+    return XX, YY
+
+
